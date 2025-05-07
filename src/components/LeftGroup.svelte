408e77e0a21f872b2fab3ef4bfc5f97f124c4e90
jericjan <cantilfrederick@gmail.com>
2025-05-07 19:33:20 +0800

    - rounded corners and removed shadow - removed battery - replaced heart with lightning bolt

--- src/components/LeftGroup.svelte
+++ src/components/LeftGroup.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
-  import type { BatteryOutput, CpuOutput, MemoryOutput } from "zebar";
+  import type { CpuOutput, MemoryOutput } from "zebar";
 
   import Button from "./Button.svelte";
   import Meter from "./Meter.svelte";
 
   type LeftGroupProps = {
-    battery: BatteryOutput;
     cpu: CpuOutput;
     memory: MemoryOutput;
   };
 
-  let { battery, cpu, memory }: LeftGroupProps = $props();
+  let { cpu, memory }: LeftGroupProps = $props();
 </script>
 
 <div class="flex flex-row gap-3 items-center">
-  <Button class="text-zb-icon" iconClass="heart-filled" />
+  <Button class="text-zb-icon" iconClass="bolt-filled" />
   <div class="flex gap-1 items-center">
     <i class="ti ti-ruler-2"></i>
     <Meter class="bg-zb-memory" percent={Math.round(memory?.usage ?? 0)} />
@@ -22,12 +21,5 @@
   <div class="flex gap-1 items-center">
     <i class="ti ti-cpu"></i>
     <Meter class="bg-zb-cpu" percent={Math.round(cpu?.usage ?? 0)} />
-  </div>
-  <div class="flex gap-1 items-center">
-    <i class="ti ti-bolt"></i>
-    <Meter
-      class="bg-zb-battery-good"
-      percent={Math.round(battery?.chargePercent ?? 100)}
-    />
   </div>
 </div>
